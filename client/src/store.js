4d2af0ed3e395b672f84a57732e098676c163f40
Ariful <arifulislam1194@gmail.com>
2019-04-23 14:25:56 +0600

    make it better

--- client/src/store.js
+++ client/src/store.js
@@ -9,10 +9,11 @@ const initialState = {};
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
-    applyMiddleware(...middlware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  compose(applyMiddleware(...middlware))
 );
 
 export default store;
+// compose(
+//   applyMiddleware(...middlware),
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// )
